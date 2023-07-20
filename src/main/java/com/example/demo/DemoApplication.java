895c82762e41a28337a82a5044a73f18f4255de4
Javier Emanuel Huebra <104619744+javierhuebra@users.noreply.github.com>
2023-07-20 13:12:57 -0300

    nuevo comit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+// 1. Annotation
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	 	// 1. Anotación
+        @RequestMapping("/")
+        // 3. Método que se asigna a la Solicitud anterior.
+        public String hello() { // 3
+                return "Hello World!";
+        }
+
+		// 1. Anotación
+        @RequestMapping("/random")
+        // 3. Método que se asigna a la Solicitud anterior.
+        public String random() { // 3
+                return "Estoy corriendo Spring Boot desde vscode, tengo bastantes dudas pero imagino que se iran aclarando a medida que transite el curso, esta muy bueno";
+        }
+
+}
